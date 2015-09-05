42c17a22717c55aea76414046bffa6daad9b495f
carloslimasis <carloslimasis@gmail.com>
2015-09-05 13:00:15 -0300

    Add archives

--- chat/chat-server-4.js
+++ chat/chat-server-4.js
@@ -11,13 +11,29 @@ chatServer.on('connection', function(client) {
   client.on('data', function(data){
     broadcast(data, client);
   });
+
+  client.on('end', function(){
+    clientList.splice(clientList.indexOf(client), 1);
+  });
+
+  client.on('error', function(e) {
+    console.log(e);
+  });
 });
 
 function broadcast(message, client) {
   for(var i = 0; i < clientList.length; i+=1) {
+    var cleanup = [];
     if(client !== clientList[i]){
       clientList[i].write(client.name + ' says ' + message);
+    } else {
+      cleanup.push(clientList[i]);
+      clientList[i].destroy();
     }
+  }
+
+  for(i = 0; i < cleanup.length; i+=1) {
+    clientList.splice(clientList.indexOf(cleanup[i]), 1);
   }
 }
 
